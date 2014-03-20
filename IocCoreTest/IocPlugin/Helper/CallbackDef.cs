1f6c3c492a11c9a18b81b31c6a3377a87ef2c07e
lyllylyq <lyllylyq@gmail.com>
2014-03-20 20:10:41 +0800

    添加了一个oracleNhibernate测试类
    没完成~~~

---
+++ IocCoreTest/IocPlugin/Helper/CallbackDef.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IocCore.IocDependency;
+using IocCore.IocCache;
+using IocCore.IocDependency.CreateArgs;
+using IocCore;
+using IocCore.IocDbOp;
+
+namespace IocCoreTest.IocPlugin.Helper
+{
+    public class CallbackDef
+    {
+        public static void OnCacheRemovedCallback(string key, object value, RemovedReason reason)
+        {
+            Console.WriteLine(String.Format("CacheItemRemoved key:{0} reason:{1}", key, reason));
+        }
+        public static IDependencyWrapper DbCacheDependencyCallback(object target, object[] args)
+        {
+            CacheDependencyWrapper dependency = new CacheDependencyWrapper();
+            try
+            {
+                if (args.Length == 2)
+                {
+                    IDependencyWrapper dbDependency = IocCoreFactory.Get<IDependencyWrapper>("DBDependency");
+                    DBDependencyCreateArgs dbArg
+                        = new DBDependencyCreateArgs() { Sql = args[0] as string, DbOp = args[1] as IDbOp };
+                    dbDependency.Create(dbArg);
+                    IDependencyWrapper dbConnDependency = IocCoreFactory.Get<IDependencyWrapper>("DBConnDependency");
+                    DBConnDependencyCreateArgs dbCoonArg
+                        = new DBConnDependencyCreateArgs() { DbOp = args[1] as IDbOp };
+                    dbConnDependency.Create(dbCoonArg);
+                    IDependencyWrapper aggregateDependency = IocCoreFactory.Get<IDependencyWrapper>("AggregateDependency");
+                    AggregateDependencyCreateArgs aggregateArg
+                        = new AggregateDependencyCreateArgs() { Wrappers = new IDependencyWrapper[] { dbDependency, dbConnDependency } };
+                    aggregateDependency.Create(aggregateArg);
+                    return aggregateDependency;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch (Exception ex)
+            {
+                //这里处理缓存依赖生成过程中的异常
+                throw ex;
+                //return null;
+            }
+
+        }
+    }
+}
---
+++ IocCoreTest/IocPlugin/OracleNHibernateTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NHibernate;
+using System.Data;
+using IocCoreTest.IocPlugin.Helper;
+using IocCore;
+using IocCore.IocDbOp;
+using IocCore.IocCache;
+using IocCore.IocDependency;
+using IocCore.IocDependency.CreateArgs;
+using IocPlugin.IocNHibernate.OracleNHibernate;
+using IocCore.IocLog;
+namespace IocCoreTest.IocPlugin
+{
+    [TestFixture]
+    public class OracleNHibernateTest
+    {
+        public class SimpleBussiness
+        {
+            public virtual IList<CITY> TestGetData()
+            {
+                using (ISession session = OracleSessionFactory.GetInstance().OpenSession())
+                {
+                    IQuery sessionCreateQuery = session.CreateQuery("from CITY");
+                    IList<CITY> pList = sessionCreateQuery.List<CITY>();
+                    return pList;
+                }
+            }
+            [Cache(KeyType = KeyType.str, Key = "select * from city", LogLevel = LogLevel.debug,
+                DependencyCallback = "CallbackDef.DbCacheDependencyCallback",
+                OnRemovedCallback = "CallbackDef.OnCacheRemovedCallback")]
+            public virtual IList<CITY> TestCacheGetData(IDbOp op)
+            {
+                using (ISession session = OracleSessionFactory.GetInstance().OpenSession())
+                {
+                    IQuery sessionCreateQuery = session.CreateQuery("from CITY");
+                    IList<CITY> pList = sessionCreateQuery.List<CITY>();
+                    return pList;
+                }
+            }
+            
+        }
+
+
+        [Test]
+        public void TestConnection()
+        {
+            using (ISession session = OracleSessionFactory.GetInstance().OpenSession())
+            {
+                ConnectionState state = session.Connection.State;
+                Assert.AreEqual(session.Connection.State, state);
+            }
+        }
+    }
+}
